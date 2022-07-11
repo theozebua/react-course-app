64bf8c75b393e748bfd4bd127296321244850497
theozebua <ronizebua.rz@gmail.com>
2022-07-11 07:37:46 +0700

    Create new Header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,28 @@
+import HeaderSVG from '../../assets/svg/header-svg.svg'
+
+export default function Header() {
+  return (
+    <header className='mt-16 lg:mt-20'>
+      <div className='container mx-auto p-2 md:px-8'>
+        <div className='flex flex-col gap-4 md:flex-row md:gap-8'>
+          <div className='flex w-full items-center justify-center md:order-1 md:w-1/2'>
+            <img className='w-full' src={HeaderSVG} alt='Header SVG' />
+          </div>
+          <div className='flex flex-col gap-2 md:w-1/2'>
+            <h1 className='text-center text-4xl leading-tight tracking-wider md:text-left'>
+              Improve your skills with <span className='font-semibold text-primary'>Online Course.</span>
+            </h1>
+            <p className='text-center leading-relaxed tracking-wide md:text-left'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt dolorem dicta ad quis temporibus facere amet, eveniet nihil molestiae quos unde officiis optio necessitatibus, accusantium veniam, voluptate vel commodi
+              dolore?
+            </p>
+            <div className='mt-2 flex items-center justify-center gap-4 md:justify-start'>
+              <button className='rounded-xl border border-primary bg-primary px-4 py-2 tracking-wide text-white'>Get Started</button>
+              <button className='rounded-xl border-2 border-primary px-4 py-2 tracking-wide text-primary'>Learn More</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
