0c7c0d6d50bba03be16a00f54432ba672f710e12
theozebua <ronizebua.rz@gmail.com>
2022-07-13 12:45:44 +0700

    Display all created components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+import Contact from './components/Contact'
+import Fab from './components/Fab'
+import Footer from './components/Footer'
 import Header from './components/Header'
 import Navbar from './components/Navbar'
+import Prices from './components/Prices'
 import Service from './components/Service'
+import WhyUs from './components/WhyUs'
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <Fab />
       <Header />
       <main>
+        <WhyUs />
         <Service />
+        <Prices />
+        <Contact />
       </main>
+      <Footer />
     </>
   )
 }
