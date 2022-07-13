e8be7f244fca7a661c7f4012a69c82ad4058c454
theozebua <ronizebua.rz@gmail.com>
2022-07-13 12:51:15 +0700

    Add id to header tag

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,7 +2,7 @@ import HeaderSVG from '../../assets/svg/header-svg.svg'
 
 export default function Header() {
   return (
-    <header className='my-16 lg:mt-20'>
+    <header className='my-16 lg:mt-20' id='home'>
       <div className='container mx-auto p-2 md:px-8'>
         <div className='flex flex-col gap-4 md:flex-row md:gap-8'>
           <div className='flex w-full items-center justify-center md:order-1 md:w-1/2'>
