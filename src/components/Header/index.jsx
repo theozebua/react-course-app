3daca7ed437b59c00a7968e745fa6ea2614b0113
theozebua <ronizebua.rz@gmail.com>
2022-07-13 12:40:09 +0700

    Modify Header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,7 +2,7 @@ import HeaderSVG from '../../assets/svg/header-svg.svg'
 
 export default function Header() {
   return (
-    <header className='mt-16 lg:mt-20'>
+    <header className='my-16 lg:mt-20'>
       <div className='container mx-auto p-2 md:px-8'>
         <div className='flex flex-col gap-4 md:flex-row md:gap-8'>
           <div className='flex w-full items-center justify-center md:order-1 md:w-1/2'>
@@ -17,8 +17,12 @@ export default function Header() {
               dolore?
             </p>
             <div className='mt-2 flex items-center justify-center gap-4 md:justify-start'>
-              <button className='rounded-xl border border-primary bg-primary px-4 py-2 tracking-wide text-white'>Get Started</button>
-              <button className='rounded-xl border-2 border-primary px-4 py-2 tracking-wide text-primary'>Learn More</button>
+              <a href='#' className='rounded-xl border border-primary bg-primary px-4 py-2 tracking-wide text-white transition duration-300 hover:shadow-lg hover:shadow-primary/50'>
+                Get Started
+              </a>
+              <a href='#' className='rounded-xl border-2 border-primary px-4 py-2 tracking-wide text-primary transition duration-300 hover:shadow-lg hover:shadow-gray-800/50'>
+                Learn More
+              </a>
             </div>
           </div>
         </div>
