411fa1541ad44b7cf4449987ef6080dd0268a683
theozebua <ronizebua.rz@gmail.com>
2022-07-14 10:06:16 +0700

    Change plain svg to object tag

--- src/components/Prices/index.jsx
+++ src/components/Prices/index.jsx
@@ -1,5 +1,7 @@
 import { CheckCircleIcon, XCircleIcon } from '@heroicons/react/outline'
 import { useEffect, useState } from 'react'
+import WavesUp from '../../assets/svg/waves-up.svg'
+import WavesDown from '../../assets/svg/waves-down.svg'
 import Button from './Button'
 import Card from './Card'
 import Title from './Title'
@@ -21,13 +23,7 @@ export default function Prices() {
 
   return (
     <>
-      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320'>
-        <path
-          fill='#312e81'
-          fillOpacity='1'
-          d='M0,32L48,42.7C96,53,192,75,288,96C384,117,480,139,576,138.7C672,139,768,117,864,106.7C960,96,1056,96,1152,122.7C1248,149,1344,203,1392,229.3L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z'
-        ></path>
-      </svg>
+      <object data={WavesUp} type='image/svg+xml'></object>
       <section className='bg-primary text-white' id='prices'>
         <div className='container mx-auto p-4 md:px-8'>
           <h3 className='mb-4 text-center text-2xl font-semibold tracking-wider md:mb-8 md:text-3xl'>Prices</h3>
@@ -55,13 +51,7 @@ export default function Prices() {
           </div>
         </div>
       </section>
-      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320'>
-        <path
-          fill='#312e81'
-          fillOpacity='1'
-          d='M0,32L48,42.7C96,53,192,75,288,96C384,117,480,139,576,138.7C672,139,768,117,864,106.7C960,96,1056,96,1152,122.7C1248,149,1344,203,1392,229.3L1440,256L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z'
-        ></path>
-      </svg>
+      <object data={WavesDown} type='image/svg+xml'></object>
     </>
   )
 }
--- src/components/WhyUs/index.jsx
+++ src/components/WhyUs/index.jsx
@@ -1,16 +1,12 @@
 import WhyUsSVG from '../../assets/svg/whyus-svg.svg'
+import WavesUp from '../../assets/svg/waves-up.svg'
+import WavesDown from '../../assets/svg/waves-down.svg'
 import Wrapper from './Wrapper'
 
 export default function WhyUs() {
   return (
     <>
-      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320'>
-        <path
-          fill='#312e81'
-          fillOpacity='1'
-          d='M0,32L48,42.7C96,53,192,75,288,96C384,117,480,139,576,138.7C672,139,768,117,864,106.7C960,96,1056,96,1152,122.7C1248,149,1344,203,1392,229.3L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z'
-        ></path>
-      </svg>
+      <object data={WavesUp} type='image/svg+xml'></object>
       <section className='bg-primary text-white' id='why-us'>
         <div className='container mx-auto p-4 md:px-8'>
           <h3 className='mb-4 text-center text-2xl font-semibold tracking-wider md:mb-8 md:text-3xl'>Why Us?</h3>
@@ -31,13 +27,7 @@ export default function WhyUs() {
           </div>
         </div>
       </section>
-      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320'>
-        <path
-          fill='#312e81'
-          fillOpacity='1'
-          d='M0,32L48,42.7C96,53,192,75,288,96C384,117,480,139,576,138.7C672,139,768,117,864,106.7C960,96,1056,96,1152,122.7C1248,149,1344,203,1392,229.3L1440,256L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z'
-        ></path>
-      </svg>
+      <object data={WavesDown} type='image/svg+xml'></object>
     </>
   )
 }
